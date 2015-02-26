6bc00884a65943ebb47b171d1840f2462f88aa8a
EvgeneVolkov <evgenevolkov@gmail.com>
2015-02-26 19:54:51 +0200

    Seems like  I've dealt with module pattern. (Ye-e-e-h-h-a-a!!) I've created a separate branch Module_pattern for this case. I'll try to merge it to release 0.0.1 after you've approved the changes. Will move on to Command Pattern while waiting for Your comments.

--- myscript.js
+++ myscript.js
@@ -1,20 +1,21 @@
 var dataArray = [1, 8, 7, [4, 65, 1, [2, 7], 45, [6, 4], 6], 4, 8];
 
-var jenyaFlatternArray = function (inputArray) {
-    var flattering = function (mlArray) {
-        for (var i = 0; i < mlArray.length; i++) {
-            var mlArrayI = mlArray[i];
-            if (typeof mlArrayI === "object") {
-                flattering(mlArrayI);
-            } else {
-                outputArray.push(mlArrayI);
-            }
-        }
-    };
+var jenyaFlatternArrayModule = (function () {
     var outputArray = [];
-    flattering(inputArray);
-    return outputArray;
-};
+    return {
+        flatter: function (mlArray) {
+            for (var i = 0; i < mlArray.length; i++) {
+                var mlArrayI = mlArray[i];
+                if (typeof mlArrayI === "object") {
+                    this.flatter(mlArrayI);
+                } else {
+                    outputArray.push(mlArrayI);
+                }
+            }
+            return outputArray;
+        }
+    }
+})();
 
 
-console.log(jenyaFlatternArray(dataArray));
+console.log(jenyaFlatternArrayModule.flatter(dataArray));
